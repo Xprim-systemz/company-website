224477a1a2146bb2ca9b1ea003ea64fe3135453d
Xprim-systemz <terese.akombo@gmail.com>
2023-08-03 20:27:17 +0200

    send email using the form plus use of select box

--- pages/Contact us.py
+++ pages/Contact us.py
@@ -7,15 +7,15 @@ df1=pd.read_csv("topics.csv")
 
 with st.form(key="contactMe"):
     user_email=st.text_input("Enter your email")
-    topics_series = []
-    for index,rows in df1.iterrows():
-        topics_series.append(rows["topic"])
-    topics_series=pd.Series(topics_series)
-    option = st.selectbox('Select topic you would like to discuss',topics_series)
-
+    # topics_series = []
+    # for index,rows in df1.iterrows():
+    #     topics_series.append(rows["topic"])
+    # topics_series=pd.Series(topics_series)
+    # option = st.selectbox('Select topic you would like to discuss',topics_series)
+    option = st.selectbox('Select topic you would like to discuss', df1["topic"])
     message=st.text_area("Type your message")
-    message=f""" {message} \nFrom: {user_email}"""
+    # message=f""" {message} \nFrom: {user_email}"""
     button=st.form_submit_button("Send Message")
     if button:
-        send_email(message,option)
+        send_email(message,option,user_email)
         st.info("Message Sent!")
--- send_email.py
+++ send_email.py
@@ -1,15 +1,17 @@
 import smtplib,ssl
+import os
 
-
-def send_email(message,topic):
+def send_email(message,topic,sender):
     host = "smtp.gmail.com"
     port = 465
     username = "terese.akombo@gmail.com"
-    password = "jnzqxbarhubwlezf"
+    password = os.getenv("PWD")
     receiver = "terese.akombo@gmail.com"
     my_context = ssl.create_default_context()
     message2 = f"""\
 Subject: Enquiry on {topic}
+
+From: {sender}
 
 Topic: {topic} 
 
