1514bb435d0d67dbe6ffabf670466e1dc4d0d25a
Xprim-systemz <terese.akombo@gmail.com>
2023-08-03 17:43:40 +0200

    send email using the form

--- pages/Contact us.py
+++ pages/Contact us.py
@@ -1,10 +1,13 @@
 import streamlit as st
+from send_email import send_email
 
 st.subheader("Contact Me")
 
 with st.form(key="contactMe"):
     user_email=st.text_input("Enter your email")
     message=st.text_area("Type your message")
+    message=f""" {message} \nFrom: {user_email}"""
     button=st.form_submit_button("Send Message")
     if button:
-        st.write("good")
+        send_email(message,user_email)
+        st.info("Message Sent!")
--- send_email.py
+++ send_email.py
@@ -1,17 +1,20 @@
 import smtplib,ssl
-host="smtp.gmail.com"
-port=465
-username="terese.akombo@gmail.com"
-password="jnzqxbarhubwlezf"
 
-receiver="terese.akombo@gmail.com"
-my_context=ssl.create_default_context()
-message="""\
-Subject: Greetings!
-Good to see and read from you
-bye!
+
+def send_email(message,sender):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "terese.akombo@gmail.com"
+    password = "jnzqxbarhubwlezf"
+    receiver = "terese.akombo@gmail.com"
+    my_context = ssl.create_default_context()
+    message2 = f"""\
+Subject: 
+{message}
 """
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message2)
 
-with smtplib.SMTP_SSL(host,port,context=my_context) as server:
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
+
+# send_email()
